4e9ff3e800746bdddca89244f2c70685744c31d1
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-23 21:25:31 +0300

    Update LoginScreen.js

--- myNewProject/screens/auth/LoginScreen.js
+++ myNewProject/screens/auth/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from 'react-redux'
 
 import { StyleSheet, View, ImageBackground, Text, TextInput, TouchableOpacity, Dimensions, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform } from "react-native";
