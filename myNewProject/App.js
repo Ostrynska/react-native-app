eeb58ac4cc6f2d14ed4372d82351ecdd4367b3a0
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-23 21:25:16 +0300

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,37 +1,19 @@
-import React, { useEffect, useCallback, useState} from "react";
+import React, { useCallback } from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
+import Main from "./components/Main";
 
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-import { Provider, useDispatch, useSelector } from 'react-redux';
-
-import { useRoute } from "./router";
+import { Provider } from 'react-redux';
 import { store } from "./redux/store";
-import { auth } from "./firebase/config";
-// import { authStateChangeUser } from "./redux/auth/authOperations";
 
 export function App()
 {
-  const [user, setUser] = useState(null);
   const [fontsLoaded] = Font.useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
   });
-  // const { stateChange } = useSelector((state) => state.auth);
-  // const { userId } = useSelector((state) => state.auth);
-  // const dispatch = useDispatch();
-
-  auth.onAuthStateChanged((user) => setUser(user));
-
-  const routing = useRoute(user);;
-  // const routing = useRoute({});
-  // const routing = useRoute(null);
-
-  // useEffect(() => {
-  //   dispatch(authStateChangeUser());
-  // }, []);
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -45,10 +27,7 @@ export function App()
 
   return (
     <Provider store={store}>
-    <NavigationContainer >
-      {routing}
-    </NavigationContainer>
+      <Main />
     </Provider>
-  );
-}
+)}
 
