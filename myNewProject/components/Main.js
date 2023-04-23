c6b7ac5000ba5b28850b1fb8ba6572b6fba1ec82
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-23 21:25:18 +0300

    Create Main.js

---
+++ myNewProject/components/Main.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+import { useRoute } from "../router";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authStateChangeUser } from "../redux/auth/authOperations";
+import { ActivityIndicator } from "react-native";
+
+const Main = () => {
+  const { stateChange } = useSelector((state) => state.auth);
+  const { userId } = useSelector((state) => state.auth);
+  const [loader, setLoader] = useState(false);
+  const dispatch = useDispatch();
+
+  const routing = useRoute(stateChange);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(true);
+    }, 500);
+    dispatch(authStateChangeUser());
+  }, []);
+
+  return !loader ? (
+    <ActivityIndicator
+      size="large"
+      color="#FF6C00"
+      style={{
+        marginTop: "100%",
+      }}
+    />
+  ) : (
+    <NavigationContainer>{routing}</NavigationContainer>
+  );
+};
+
+export default Main;
