523aa320d0877cdb16f23e00b20bd1d6613ddddd
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-25 20:40:32 +0300

    Update PostsScreen.js

--- myNewProject/screens/main/PostsScreen.js
+++ myNewProject/screens/main/PostsScreen.js
@@ -1,144 +1,27 @@
-import React, { useState, useEffect} from "react";
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { moduleName } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../nestedScreen/HomeScreen";
+import CommentsScreen from "../nestedScreen/CommentsScreen";
+import MapScreen from "../nestedScreen/MapScreen";
 
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image} from "react-native";
 
-import { Feather } from '@expo/vector-icons';
+const NestedScreen = createStackNavigator();
 
-import { authSignOutUser } from "../../redux/auth/authOperations";
-import authSelectors from "../../redux/auth/authSelectors";
-
-const PostsScreen = ({ navigation, route }) =>
-{
-    const [posts, setPosts] = useState([]);
-    const user = useSelector(authSelectors.getUser);
-
-    const dispatch = useDispatch(); 
-
-    const signOut = () =>
-    {
-    dispatch(authSignOutUser());
-    };
-
-    useEffect(() => {
-        if (route.params) {
-            setPosts((prevState) => [...prevState, route.params]);
-        }
-    }, [route.params]);
-
-    useEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <TouchableOpacity onPress={signOut} style={{marginRight: 16}}>
-                    <Feather name="log-out" size={24} color="#BDBDBD"/>
-                </TouchableOpacity>
-            ),
-        });
-    }, [navigation]);
-
-    return(
-        <View style={styles.container}>
-            <View style={styles.innerBox}>
-                {user.userPhoto !== null ? <Image style={styles.avatarBox} source={{uri: user.userPhoto}}/> : <View style={styles.avatarBox}></View>}
-                <View style={styles.infoBox}>
-                    <Text style={styles.user}>{user.nickname}</Text>
-                    <Text style={styles.email}>{user.email}</Text>
-                </View>
-            </View>
-            <View style={{marginHorizontal: 16}}>
-            <FlatList
-                data={posts}
-                keyExtractor={(item, indx) => indx.toString()}
-                renderItem={({ item }) => (
-                <View
-                    style={{
-                    marginBottom: 34,
-                    justifyContent: "center",
-                    // alignItems: "center",
-                    }}
-                >
-                    <Image
-                    source={{ uri: item.photo }}
-                    style={{ width: '100%', height: 240, borderRadius: 8 }}
-                        />
-                        <Text style={styles.titlePost}></Text>
-                        <View style={{ marginTop: 11, flexDirection: 'row'}}>
-                            <View style={{ flexDirection: "row-reverse", alignItems: 'center' }}>
-                                <Text style={styles.commentsCount}>0</Text>
-                                <Feather name="message-circle" size={16} color="#BDBDBD"  />
-
-                            </View>
-                            <View style={{flexDirection: "row", marginLeft: 'auto', alignItems: 'center'}}>
-                                <Feather name="map-pin" size={16} color="#BDBDBD" style={{right: 8}} />
-                                <Text style={styles.location}></Text>
-                            </View>
-                        </View>
-                </View>
-                )}
-                />
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
-    innerBox: {
-        marginHorizontal: 16,
-        marginVertical: 32,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-    },
-    avatarBox: {
-        backgroundColor: "#BDBDBD",
-        borderRadius: 16,
-        width: 60,
-        height: 60,
-    },
-    infoBox: {
-        marginLeft: 10,
-        justifyContent: 'center',
-    },
-    user: {
-        fontFamily: "Roboto-Medium",
-        fontSize: 13,
-        lineHeight: 15,
-        color: "#212121",
-    },
-    email: {
-        fontFamily: "Roboto-Regular",
-        fontSize: 11,
-        lineHeight: 13,
-        color: "#212121CC",
-    },
-    titlePost: {
-        marginTop: 8,
-        fontFamily: "Roboto-Medium",
-        fontSize: 16,
-        lineHeight: 19,
-        color: '#212121',
-    },
-    commentsCount: {
-        fontFamily: 'Roboto-Regular',
-        fontWeight: 400,
-        fontSize: 16,
-        lineHeight: 19,
-        color: '#BDBDBD',
-        right: 6,
-    },
-    location: {
-        alignItems: 'flex-end',
-        fontFamily: 'Roboto-Regular',
-        fontWeight: 400,
-        fontSize: 16,
-        lineHeight: 19,
-        textAlign: 'right',
-        textDecorationLine: 'underline',
-        color: '#212121',
-    },
-})
+const PostsScreen = () => {
+  return (
+    <NestedScreen.Navigator>
+      <NestedScreen.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <NestedScreen.Screen name="Comments" component={CommentsScreen} />
+      <NestedScreen.Screen name="Map" component={MapScreen} />
+    </NestedScreen.Navigator>
+  );
+};
 
 export default PostsScreen;
