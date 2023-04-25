45ac3876004c1f6a0e793df3e815d216b71482f2
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-25 20:22:31 +0300

    Update CommentsScreen.js

---
+++ myNewProject/screens/nestedScreen/CommentsScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const CommentsScreen = () => (
+  <View style={styles.container}>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default CommentsScreen;
