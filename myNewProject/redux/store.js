c6e6b8e07eba74ac3f694fbc84f39ab23bb3fa7e
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-26 11:31:44 +0300

    Update store.js

--- myNewProject/redux/store.js
+++ myNewProject/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import { authReducer } from "./auth/authReducer";
-// import { postsReduser } from "./posts/postsSlice";
+import { postsReducer } from "./posts/postsReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-//   posts: postsReduser,
+  posts: postsReducer,
 });
 
 export const store = configureStore({
