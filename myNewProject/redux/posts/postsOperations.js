a835988d86179aefd602bd545bdcd2efc0340435
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-26 16:58:21 +0300

    Update postsOperations.js

--- myNewProject/redux/posts/postsOperations.js
+++ myNewProject/redux/posts/postsOperations.js
@@ -1,95 +1,90 @@
-import {
-  addDoc,
-  collection,
-  getDocs,
-  query,
-  getCountFromServer,
-  where,
-  doc,
-} from 'firebase/firestore';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from '../../firebase/config';
-
-import { postsAction } from './postsReducer';
 
 import { Alert } from "react-native";
 
-export const getAllPosts = () => async (dispatch, getState) => {
-  try {
-    const { userId } = getState().auth;
-
-    // get all posts
-    const posts = await getDocs(collection(db, 'posts'));
-    console.log('posts::', posts);
-
-    // add id to collection and count comments
-    const newPosts = posts.docs.map(async doc => {
-      // Get comments count
-    //   const snapshotComments = await getCountFromServer(
-    //     collection(doc.ref, 'comments'),
-    //   );
-    //   const countComments = snapshotComments.data().count;
-
-      return {
-        ...doc.data(),
-        id: doc.id,
-        // countComments,
+export const getAllPosts = createAsyncThunk(
+  "posts/getAllPosts",
+  async (_, thunkAPI) => {
+    try {
+      const getPosts = async () => {
+        const querySnapshot = await getDocs(collection(db, "posts"));
+        const posts = [];
+        querySnapshot.forEach((doc) => {
+          posts.push(doc.data());
+        });
+        return posts;
       };
-    });
-
-    // Resolve all promises
-    const payload = await Promise.all(newPosts);
-
-    dispatch(postsAction.updatePosts(payload));
+      const posts = await getPosts();
+      return posts.sort(
+        (firstPost, lastPost) => lastPost.createdAt - firstPost.createdAt
+      );
   } catch (error) {
       Alert.alert(error.message);
-      console.log(error);
+      return thunkAPI.rejectWithValue(err.message);
+    }
   }
-};
+);
 
-export const getOwnPosts = () => async (dispatch, getState) => {
-  try {
-    const { userId } = getState().auth;
-    const q = query(collection(db, 'posts'), where('userId', '==', userId));
-    const posts = await getDocs(q);
+export const getOwnPosts = createAsyncThunk(
+  "posts/getOwnPosts",
+  async (uid, thunkAPI) => {
+    try {
+      const getPosts = async () => {
+        const posts = [];
+        const q = query(collection(db, "posts"), where("userId", "==", uid));
 
-    // add id to collection and count comments
-    const newPosts = posts.docs.map(async doc => {
-      // Get comments count
-    //   const snapshotComments = await getCountFromServer(
-    //     collection(doc.ref, 'comments'),
-    //   );
-    //   const countComments = snapshotComments.data().count;
-
-    
-      return {
-        ...doc.data(),
-        id: doc.id,
-        // countComments,
-      };
-    });
-
-    // Resolve all promises
-    const payload = await Promise.all(newPosts);
-
-    dispatch(postsAction.updateOwnPosts(payload));
-  } catch (error) {
-      Alert.alert(error.message);
-      console.log(error);
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            posts.push(doc.data());
+        });
+        return posts.sort(
+            (firstPost, lastPost) => lastPost.createdAt - firstPost.createdAt
+        );
+        };
+        const posts = await getPosts();
+        return posts;
+    } catch (err) {
+        Alert.alert(error.message);
+        return thunkAPI.rejectWithValue(err.message);
+    }
   }
-};
+);
 
-export const uploadPostToServer = post => async (dispatch, getState) => {
-  console.log('post::', post);
-  const { userId } = getState().auth;
-  try {
-    await addDoc(collection(db, 'posts'), {
-      ...post,
-      userId,
-    });
-    dispatch(getAllPosts());
-    dispatch(getOwnPosts());
-  } catch (e) {
-      Alert.alert(error.message);
-      console.log(error);
-  }
-};
+export const addPost = createAsyncThunk(
+  "posts/addPost",
+  async (post, thunkAPI) => {
+      try {
+          return post;
+      } catch (error) {
+        Alert.alert(error.message);
+        return thunkAPI.rejectWithValue(err.message);
+    }
+}
+);
+
+// export const fetchPostCommnets = createAsyncThunk(
+//   "posts/fetchPostComments",
+//   async (id, thunkAPI) => {
+//     try {
+//       const getPosts = async () => {
+//         const comments = [];
+//         const q = query(collection(db, "comments"), where("postId", "==", id));
+
+//         const querySnapshot = await getDocs(q);
+//         querySnapshot.forEach((doc) => {
+//           comments.push(doc.data());
+//         });
+//         return comments;
+//       };
+//       const comments = await getPosts();
+//       return comments.sort(
+//         (firstComment, lastComment) =>
+//           lastComment.createdAt - firstComment.createdAt
+//       );
+//     } catch (err) {
+//       return thunkAPI.rejectWithValue(err.message);
+//     }
+//   }
+// );
