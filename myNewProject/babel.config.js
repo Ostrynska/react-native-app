0a2ce0428712f18fbe53b54e77dea6eca8fcf29a
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-03 11:10:30 +0300

    Update babel.config.js

--- myNewProject/babel.config.js
+++ myNewProject/babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@src': './src',
+            '@res': './res',
+          },
+        },
+      ],
+    ],
   };
 };
